59d629b9ecbb736a8cab63f566c52423e0948289
Tiago Martins Araujo <tiago.martins.araujo@gmail.com>
2016-09-29 07:52:23 -0300

    change routes for ember standarts

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :hotels
-  resources :accomodation_types, only: [:index, :show]
+  # resources :accomodation_types, only: [:index, :show]
+  get 'accomodation-types', to: 'accomodation_types#index'
+  get 'accomodation_types/:id', to: 'accomodation_types#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
